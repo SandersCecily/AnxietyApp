21d686367e4a255fb19aad05c5019e6122796282
Cecily <Sanders.Cecily@gmail.com>
2019-04-24 20:43:56 -0400

    added build

---
+++ client/build/precache-manifest.393a742c4ea634cdef9c713acdf86a44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39a2e4dcb105586177f13c293fafb94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8df2bd38bb5524e47873",
+    "url": "/static/css/main.39249d0d.chunk.css"
+  },
+  {
+    "revision": "2343fd2c63cadbd7261d",
+    "url": "/static/js/2.e357aed0.chunk.js"
+  },
+  {
+    "revision": "8df2bd38bb5524e47873",
+    "url": "/static/js/main.d02909fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
