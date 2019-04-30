42467006e29fcdf62b85fb71071f1839ec4df7f8
Cecily <Sanders.Cecily@gmail.com>
2019-04-30 14:29:27 -0400

    adding build to last commit

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d5343d4c468f78f234fd5ef684e08c6b.js"
+  "/precache-manifest.4bb3b076aa1b6f881a6d567d1872b029.js"
 );
 
 self.addEventListener('message', (event) => {
