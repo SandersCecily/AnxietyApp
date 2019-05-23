f4fda4c1452363a8695ac395c918c2835881dae7
Cecily <Sanders.Cecily@gmail.com>
2019-05-23 03:05:02 -0400

    another syntax change

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use('/', express.static(path.join(__dirname, '/build')));
+    app.use('/', express.static(path.join(__dirname, 'client/build')));
 }
 
 app.listen(PORT, function () {
