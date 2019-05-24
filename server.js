64613dd4791a843a3d255bfc7737fa2b03f0d9b4
Cecily <Sanders.Cecily@gmail.com>
2019-05-24 17:46:47 -0400

    added scripts

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// const path = require("path");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const mongoose = require("mongoose");
 const routes = require("./client/routes");
@@ -31,6 +31,9 @@ app.use(routes);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
+    app.get("*", (req, res)=>{
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    })
 }
 
 app.listen(PORT, function () {
